eaa8c436a7c4373efec22cfc4e4c13b8761b1ab3
naresh546nk <naresh546.nk@gmail.com>
2022-08-06 17:27:33 +0530

    added dynamodb

--- ordersapi/src/main/java/com/aws/lambda/app/CreateOrderLambda.java
+++ ordersapi/src/main/java/com/aws/lambda/app/CreateOrderLambda.java
@@ -12,27 +12,24 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CreateOrderLambda {
-	public APIGatewayProxyResponseEvent createOrder(APIGatewayProxyRequestEvent request) throws JsonMappingException, JsonProcessingException {
-		ObjectMapper objectMapper =new ObjectMapper();
-		
+	public APIGatewayProxyResponseEvent createOrder(APIGatewayProxyRequestEvent request)
+			throws JsonMappingException, JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+
 		Order order = objectMapper.readValue(request.getBody(), Order.class);
+
+		DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+		Table table = dynamoDB.getTable(System.getenv("ORDERS_TABLE"));
+		
+		Item item = new Item()
+				.withPrimaryKey("id", order.id)
+				.withString("itemName", order.itemName)
+				.withInt("quantity",order.quantity);
 		
 		
-		/*
-		 * DynamoDB dynamoDB = new
-		 * DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
-		 * 
-		 * Table table = dynamoDB.getTable(System.getenv("ORDERS_TABLE"));
-		 * 
-		 * Item item=new Item() .withPrimaryKey("id",order.id) .withString("itemName",
-		 * order.itemName) .withInt("quentiry", order.quantity);
-		 * 
-		 * table.putItem(item);
-		 * 
-		 */
-		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Order ID:"+order.id);
-		
-		
+		table.putItem(item);
+		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Order ID:" + order.id);
+
 	}
 
 }
--- ordersapi/src/main/java/com/aws/lambda/app/ReadOrderLambda.java
+++ ordersapi/src/main/java/com/aws/lambda/app/ReadOrderLambda.java
@@ -1,5 +1,12 @@
 package com.aws.lambda.app;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.aws.lambda.model.Order;
@@ -8,31 +15,33 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ReadOrderLambda {
-	public APIGatewayProxyResponseEvent getOrder(APIGatewayProxyRequestEvent request) throws JsonMappingException, JsonProcessingException {
-		
+	public APIGatewayProxyResponseEvent getOrder(APIGatewayProxyRequestEvent request)
+			throws JsonMappingException, JsonProcessingException {
+
 		System.out.println("Executing the ReadOrderLambda method: ");
-		ObjectMapper objectMapper =new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		Order order = new Order();
+		order.id = 123;
+		order.itemName = "Mack books 13 pro";
+		order.quantity = 100;
+
 		
-		Order order =new Order();
-		order.id=123;
-		order.itemName="Mack books 13 pro";
-		order.quantity=100;
-		
-		/*
-		 * AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
-		 * ScanResult scan = dynamoDB.scan(new
-		 * ScanRequest().withTableName(System.getenv("ORDERS_TABLE"))); List<Order>
-		 * Orders = scan.getItems().stream().map(item->new Order(
-		 * Integer.parseInt(item.get("id").getN()), item.get("itemName").getS(),
-		 * Integer.parseInt(item.get("quantity").getN()) ))
-		 * .collect(Collectors.toList());
-		 */
-		
-		String orderJson = objectMapper.writeValueAsString(order);
-		
+		  AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+		  ScanResult scan = dynamoDB.scan(new
+		  ScanRequest().withTableName(System.getenv("ORDERS_TABLE")));
+		  
+		  List<Order> Orders = scan.getItems().stream().map(item->new Order(
+		  Integer.parseInt(item.get("id").getN()), 
+		  item.get("itemName").getS(),
+		  Integer.parseInt(item.get("quantity").getN())))
+		  .collect(Collectors.toList());
+		 
+
+		String orderJson = objectMapper.writeValueAsString(Orders);
+
 		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(orderJson);
-		
-		
+
 	}
 
 }
